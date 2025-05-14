c324c1ebb4546a60699e55c9356a7e960e3aad6e
xieyuhan <xieyuhan@xieyuhandeMacBook-Pro.local>
2025-05-14 11:01:06 +0200

    fix

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
 
-const InterviewCard = ({interviewId,userId,role,type,techstack,createdAt}:InterviewCardProps) => {
+const InterviewCard = ({interviewId,role,type,techstack,createdAt}:InterviewCardProps) => {
     const feedback= null as Feedback|null;
     const normalizedType=/mix/gi.test(type)? "Mixed":type;
     const formattedDate=dayjs(feedback?.createdAt||createdAt||Date.now()).format("MMM D,YYYY");
